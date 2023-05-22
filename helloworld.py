4e658dd518766d333ab99eb8c601336b19e223e7
docmalitt <doc.malitt@protonmail.com>
2023-05-22 15:13:23 +0200

    made first/last (input) name thingy... plus f string

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,12 @@
-print("Hello World 1 Hello World 11")
-print("Hello World 2")
-print("Hello World 3")
+# print("Hello World 1 Hello World 11")
+# print("Hello World 2")
+# print("Hello World 3")
+
+fname = input("What is your 1st name? ")
+lname = input("What is your 2nd (last) name? ")
+
+print("Your full name is " + fname + " " + lname)
+
+
+
+
