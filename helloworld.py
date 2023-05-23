fae7c3a15926021ed4ab247eb19f2e7149509678
hkondres <hrvoje.kondres@gmail.com>
2023-05-23 10:18:33 +0200

    2 ways of printing variables

--- helloworld.py
+++ helloworld.py
@@ -2,7 +2,12 @@
 # print("Hello World 2")
 # print("Hello World 3")
 
+# var names for first and last name
 fname = input("What is your 1st name? ")
 lname = input("What is your 2nd (last) name? ")
 
+# 1st way to print the var output
+print("Your name is " + fname + " " + lname)
+
+# 2nd way to print the var output
 print(f"Your full name is {fname} {lname}")
