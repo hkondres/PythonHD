2345d00f06b6d5b1cb991134d6dc23210ad4d1ad
hkondres <hrvoje.kondres@gmail.com>
2023-05-29 23:30:23 +0200

    made FizzBuzz script into function intro...

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,15 +2,25 @@
 # If number is divisible by 3 write Fizz
 # If number is divisible by 5 write Buzz
 # If number is divisible by both then write FizzBuzz
+import time
 
-for i in range(1,100):
-    if i % 3 == 0 and i % 5 == 0:
-        print(str(i) + " YES FizzBuzz")
-    elif i % 5 == 0:
-        print(str(i) + " Buzz")
-    elif i % 3 == 0:
-        print(str(i) + " Fizz")
-    else:
-        print(i)
+choice = int(input("What is your number of choice? "))
 
+def function(choice):
+    for i in range(1,choice):
+        if i % 3 == 0 and i % 5 == 0:
+            print(str(i) + " YES... FizzBuzz!")
+        elif i % 5 == 0:
+            print(str(i) + " Buzz")
+        elif i % 3 == 0:
+            print(str(i) + " Fizz")
+        else:
+            print(i)
 
+print("...about to run the program...")
+
+for i in range (1,6):
+    time.sleep(1)
+    print(".")
+
+function(choice)
